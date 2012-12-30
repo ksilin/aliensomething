24dfd02274adaa7d61b4268df7bcd7e03d69561e
kostja <konstantin@silin.info>
2012-12-30 15:45:41 +0100

    added avatar, projectiles, enemies

--- js/engine.js
+++ js/engine.js
@@ -1,50 +1,52 @@
-var Game = new function() {
+var Game = new function () {
 
     // Game Initialization
-    this.initialize = function(canvasElementId,sprite_data,callback) {
+    this.initialize = function (canvasElementId, sprite_data, callback) {
         this.canvas = document.getElementById(canvasElementId);
         this.width = this.canvas.width;
-        this.height= this.canvas.height;
+        this.height = this.canvas.height;
 
         this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
-        if(!this.ctx) { return alert("Please upgrade your browser to play"); }
+        if (!this.ctx) {
+            return alert("Please upgrade your browser to play");
+        }
 
         this.setupInput();
 
         this.loop();
 
-        SpriteSheet.load(sprite_data,callback);
+        SpriteSheet.load(sprite_data, callback);
     };
 
     // Handle Input
-    var KEY_CODES = { 37:'left', 39:'right', 32 :'fire' };
+    var KEY_CODES = { 37: 'left', 39: 'right', 32: 'fire' };
     this.keys = {};
 
-    this.setupInput = function() {
-        window.addEventListener('keydown',function(event) {
-            if(KEY_CODES[event.keyCode]) {
+    this.setupInput = function () {
+        window.addEventListener('keydown', function (event) {
+            if (KEY_CODES[event.keyCode]) {
                 Game.keys[KEY_CODES[event.keyCode]] = true;
                 event.preventDefault();
             }
-        },false);
+        }, false);
 
-        window.addEventListener('keyup',function(event) {
-            if(KEY_CODES[event.keyCode]) {
+        window.addEventListener('keyup', function (event) {
+            if (KEY_CODES[event.keyCode]) {
                 Game.keys[KEY_CODES[event.keyCode]] = false;
                 event.preventDefault();
             }
-        },false);
+        }, false);
     };
 
     // Game Loop
     var boards = [];
 
-    this.loop = function() {
+    this.loop = function () {
         //we are just assuming that this is our frame length in ms., based on the timeout settings
         var dt = 30 / 1000;
 
-        for(var i=0,len = boards.length;i<len;i++) {
-            if(boards[i]) {
+        for (var i = 0, len = boards.length; i < len; i++) {
+            if (boards[i]) {
                 boards[i].step(dt);
                 boards[i].draw(Game.ctx);
             }
@@ -52,28 +54,30 @@ var Game = new function() {
         // defining a one.time callback
         // the execution environment is not available at callback, so we cannot use this.loop here
         // although honestly i cannot understand why it is important where the function ref comes from
-        // TODO: and what about the execution time of the function - is it just appended t the 30ms?
-        setTimeout(Game.loop,30);
+        // TODO: and what about the execution time of the function - is it just appended to the 30ms?
+        setTimeout(Game.loop, 30);
     };
 
     // Change an active game board
-    this.setBoard = function(num,board) { boards[num] = board; };
+    this.setBoard = function (num, board) {
+        boards[num] = board;
+    };
 };
 
 
-var SpriteSheet = new function() {
+var SpriteSheet = new function () {
     this.map = { };
 
-    this.load = function(spriteData,callback) {
+    this.load = function (spriteData, callback) {
         this.map = spriteData;
         this.image = new Image();
         this.image.onload = callback;
         this.image.src = 'img/sprites.png';
     };
 
-    this.draw = function(ctx,sprite,x,y,frame) {
+    this.draw = function (ctx, sprite, x, y, frame) {
         var s = this.map[sprite];
-        if(!frame) frame = 0;
+        if (!frame) frame = 0;
         ctx.drawImage(this.image,
             s.sx + frame * s.w,
             s.sy,
@@ -84,24 +88,105 @@ var SpriteSheet = new function() {
 };
 
 // just displaying the title and subtitle
-var TitleScreen = function TitleScreen(title,subtitle,callback) {
+var TitleScreen = function TitleScreen(title, subtitle, callback) {
     var up = false;
 
-    this.step = function(dt) {
-        if(!Game.keys['fire']) up = true;
-        if(up && Game.keys['fire'] && callback) callback();
+    this.step = function (dt) {
+        if (!Game.keys['fire']) up = true;
+        if (up && Game.keys['fire'] && callback) callback();
     };
 
-    this.draw = function(ctx) {
+    this.draw = function (ctx) {
 
         // white centered text
         ctx.fillStyle = "#FFFFFF";
         ctx.textAlign = "center";
 
         ctx.font = "bold 40px bangers";
-        ctx.fillText(title,Game.width/2,Game.height/2);
+        ctx.fillText(title, Game.width / 2, Game.height / 2);
 
         ctx.font = "bold 20px bangers";
-        ctx.fillText(subtitle,Game.width/2,Game.height/2 + 40);
+        ctx.fillText(subtitle, Game.width / 2, Game.height / 2 + 40);
+    };
+};
+
+var GameBoard = function () {
+    var board = this;
+// The current list of objects
+    this.objects = [];
+    this.cnt = [];
+// Add a new object to the object list
+    this.add = function (obj) {
+        obj.board = this;
+        this.objects.push(obj);
+        this.cnt[obj.type] = (this.cnt[obj.type] || 0) + 1;
+        return obj;
+    };
+    // Mark an object for removal
+    this.remove = function (obj) {
+        var wasStillAlive = this.removed.indexOf(obj) != -1;
+        if (wasStillAlive) {
+            this.removed.push(obj);
+        }
+        return wasStillAlive;
+    };
+// Reset the list of removed objects
+    this.resetRemoved = function () {
+        this.removed = [];
+    };
+
+// Remove objects marked for removal from the list
+    this.finalizeRemoved = function () {
+        for (var i = 0, len = this.removed.length; i < len; i++) {
+            var idx = this.objects.indexOf(this.removed[i]);
+            if (idx != -1) {
+                this.cnt[this.removed[i].type]--;
+                this.objects.splice(idx, 1);
+            }
+        }
+    };
+
+// Call the same method on all current objects
+    this.iterate = function (funcName) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        for (var i = 0, len = this.objects.length; i < len; i++) {
+            var obj = this.objects[i];
+            obj[funcName].apply(obj, args)
+        }
+    };
+
+// Find the first object for which func is true
+    this.detect = function (func) {
+        for (var i = 0, val = null, len = this.objects.length; i < len; i++) {
+            if (func.call(this.objects[i])) return this.objects[i];
+        }
+        return false;
+    };
+
+// Call step on all objects and then delete
+// any objects that have been marked for removal
+    this.step = function (dt) {
+        this.resetRemoved();
+        this.iterate('step', dt);
+        this.finalizeRemoved();
+    };
+
+// Draw all the objects
+    this.draw = function (ctx) {
+        this.iterate('draw', ctx);
+    };
+
+    this.overlap = function (o1, o2) {
+        return !((o1.y + o1.h - 1 < o2.y) || (o1.y > o2.y + o2.h - 1) ||
+            (o1.x + o1.w - 1 < o2.x) || (o1.x > o2.x + o2.w - 1));
+    };
+
+    this.collide = function (obj, type) {
+        return this.detect(function () {
+            if (obj != this) {
+                var col = (!type || this.type & type) && board.overlap(obj, this);
+                return col ? this : false;
+            }
+        });
     };
 };
